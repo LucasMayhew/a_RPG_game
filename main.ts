e462e94dcaecb88cf0e7341afe28f9a5112e08aa
LucasMayhew <59377840+LucasMayhew@users.noreply.github.com>
2020-03-30 16:54:20 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,528 @@
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+`
+    //% blockIdentity=images._tile
+    export const tile2 = img`
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`
+    //% blockIdentity=images._tile
+    export const tile3 = img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+e e f f e e e e e e e f f e e e 
+e e e f f e e e e e f f e e e e 
+e e e e f f e e e f f e e e e e 
+`
+    //% blockIdentity=images._tile
+    export const tile4 = img`
+e e f f e e e e e e e f f e e e 
+e e e f f e e e e e f f e e e e 
+e e e e f f e e e f f e e e e e 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`
+}
+function enmy_span () {
+    list = tiles.getTilesByType(sprites.castle.tileGrass3)
+    for (let value of list) {
+        mySprite2 = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . b b b b b . . . . . . . 
+. . . b b b b b b b . . . . . . 
+. . . 1 1 b b b 1 1 . . . . . . 
+. . . 1 f 1 1 1 f 1 . . . . . . 
+. . . 1 f f 1 f f 1 . . . . . . 
+. . . 1 1 1 1 1 1 1 . . . . . . 
+. . . . 1 f 1 f 1 . . . . . . . 
+. . . . 1 f 1 f 1 . . . . . . . 
+. . . . . 1 1 1 . . . . . . . . 
+. e e e e e e e e e e e . . . . 
+. e e e e e e e e e e e . . . . 
+. 1 . e e e e e e e . 1 . . . . 
+. 1 . e e e e e e e . 1 . . . . 
+. 1 . b b b b b b b . 1 . . . . 
+. 1 . e e e e e e e . 1 . . . . 
+. . . e e e e e e e . . . . . . 
+. . . e 1 e . e 1 e . . . . . . 
+. . . 1 1 1 . 1 1 1 . . . . . . 
+. . . 1 1 1 . 1 1 1 . . . . . . 
+. . . e e e . e e e . . . . . . 
+. . e e e e . e e e e . . . . . 
+f f f f f f f f f f f f f f f f 
+f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+f f f f f f f f f f f f f f f f 
+`, SpriteKind.Enemy)
+        tiles.placeOnTile(mySprite2, value)
+        mySprite2.follow(mySprite, 50)
+    }
+}
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (controller.B.isPressed()) {
+        mySprite.startEffect(effects.trail, 500)
+        for (let index = 0; index < 20; index++) {
+            mySprite.y += 1
+            pause(1)
+        }
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    if (sprite.x < otherSprite.x) {
+        if (controller.B.isPressed()) {
+            if (ww == 2) {
+                otherSprite.destroy()
+            } else {
+                otherSprite.destroy()
+                music.jumpDown.play()
+                info.changeLifeBy(-1)
+            }
+        } else {
+            otherSprite.destroy()
+            music.jumpDown.play()
+            info.changeLifeBy(-1)
+        }
+    } else {
+        if (sprite.x > otherSprite.x) {
+            if (controller.B.isPressed()) {
+                if (ww == 1) {
+                    otherSprite.destroy()
+                } else {
+                    otherSprite.destroy()
+                    music.jumpDown.play()
+                    info.changeLifeBy(-1)
+                }
+            } else {
+                otherSprite.destroy()
+                music.jumpDown.play()
+                info.changeLifeBy(-1)
+            }
+        }
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (!(controller.B.isPressed())) {
+        if (ww == 2) {
+            if_waking = true
+            mySprite.setImage(img`
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . e e e e . . . . . . . . . 
+. . . . . . e e e e e e . . . . . . . . 
+. . . . . . e e d e d e . . . . . . . . 
+. . . . . . e d d d d d . . . . . . . . 
+. . . . . . d d d d d d . . . . . . . . 
+. . . . . . d d d d d d . . . . . . . . 
+. . . . . . . . d d . . . . . . . . . b 
+. . . . e e e e e e e e e e . . . . b . 
+. . . . e e e e e e e e e e . . . b . . 
+. . . . d . e e e e e e . d . . b . . . 
+. . . . d . e e e e e e . b . b . . . . 
+. . . . d . e e e e e e . d b . . . . . 
+. . . . d . e e e e e e . b . b . . . . 
+. . . . . . b b 5 5 b b . . . . . . . . 
+. . . . . . 6 6 6 6 6 6 . . . . . . . . 
+. . . . . . 6 6 . . 6 6 . . . . . . . . 
+. . . . . . 6 6 . . 6 6 . . . . . . . . 
+. . . . . e e e . . e e e . . . . . . . 
+. . . . e e e e . . e e e e . . . . . . 
+`)
+            pause(100)
+            mySprite.setImage(img`
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . e e e e . . . . . . . . . . 
+. . . . . e e e e e e . . . . . . . . . 
+. . . . . e e d e d e . . . . . . . . . 
+. . . . . e d d d d d . . . . . . . . . 
+. . . . . d d d d d d . . . . . . . . . 
+. . . . . d d d d d d . . . . . . . . . 
+. . . . . . . d d . . . . . . . . . . . 
+. . . e e e e e e e e e e . . . . . . . 
+. . . e e e e e e e e e e . . . . . . . 
+. . . d . e e e e e e . d . . . . . . . 
+. . . d . e e e e e e . d . . . . . . . 
+. . . d . e e e e e e . d b . . . . . . 
+. . . d . e e e e e e . d b b b b b b b 
+. . . . . b b 5 5 b b . . b . . . . . . 
+. . . . . 6 6 6 6 6 6 . . . . . . . . . 
+. . . . . 6 6 . . 6 6 . . . . . . . . . 
+. . . . . 6 6 . . 6 6 . . . . . . . . . 
+. . . . e e e . . e e e . . . . . . . . 
+. . . e e e e . . e e e e . . . . . . . 
+`)
+            pause(100)
+            mySprite.setImage(img`
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . e e e e . . . . . . . . . . . 
+. . . . e e e e e e . . . . . . . . . . 
+. . . . e e d e d e . . . . . . . . . . 
+. . . . e d d d d d . . . . . . . . . . 
+. . . . d d d d d d . . . . . . . . . . 
+. . . . d d d d d d . . . . . . . . . . 
+. . . . . . d d . . . . . . . . . . . . 
+. . e e e e e e e e e e . . . . . . . . 
+. . e e e e e e e e e e . . . . . . . . 
+. . d . e e e e e e . d . . . . . . . . 
+. . d . e e e e e e . d . . . . . . . . 
+. . d . e e e e e e . d . b . . . . . . 
+. . d . e e e e e e . d b . . . . . . . 
+. . . . b b 5 5 b b . b . b . . . . . . 
+. . . . 6 6 6 6 6 6 . . . . b . . . . . 
+. . . . 6 6 . . 6 6 . . . . . b . . . . 
+. . . . 6 6 . . 6 6 . . . . . . b . . . 
+. . . e e e . . e e e . . . . . . . . . 
+. . e e e e . . e e e e . . . . . . . . 
+`)
+            pause(100)
+            if_waking = false
+            mySprite.setImage(img`
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . e e e e . . . . . . . . . . . 
+. . . . e e e e e e . . . . . . . . . . 
+. . . . e e d e d e . . . . . . . . . . 
+. . . . e d d d d d . . . . . . . . . . 
+. . . . d d d d d d . . . . . . . . . . 
+. . . . d d d d d d . . . . . . . . . . 
+. . . . . . d d . . . . . . . . . . . . 
+. . e e e e e e e e e e . . . . . . . . 
+. . e e e e e e e e e e . . . . . . . . 
+. . d . e e e e e e . d . . . . . . . . 
+. . d . e e e e e e . d . . . . . . . . 
+. . d . e e e e e e . d . . . . . . . . 
+. . d . e e e e e e . d . . . . . . . . 
+. . . . b b 5 5 b b . . . . . . . . . . 
+. . . . 6 6 6 6 6 6 . . . . . . . . . . 
+. . . . 6 6 . . 6 6 . . . . . . . . . . 
+. . . . 6 6 . . 6 6 . . . . . . . . . . 
+. . . e e e . . e e e . . . . . . . . . 
+. . e e e e . . e e e e . . . . . . . . 
+`)
+            pause(100)
+        } else {
+            if (ww == 1) {
+                if_waking = true
+                mySprite.setImage(img`
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . e e e e . . . . . . . 
+. . . . . . . . e e e e e e . . . . . . 
+. . . . . . . . e d e d e e . . . . . . 
+. . . . . . . . d d d d d e . . . . . . 
+. . . . . . . . d d d d d d . . . . . . 
+. . . . . . . . d d d d d d . . . . . . 
+b . . . . . . . . . d d . . . . . . . . 
+. b . . . . e e e e e e e e e e . . . . 
+. . b . . . e e e e e e e e e e . . . . 
+. . . b . . d . e e e e e e . d . . . . 
+. . . . b . b . e e e e e e . d . . . . 
+. . . . . b d . e e e e e e . d . . . . 
+. . . . b . b . e e e e e e . d . . . . 
+. . . . . . . . b b 5 5 b b . . . . . . 
+. . . . . . . . 6 6 6 6 6 6 . . . . . . 
+. . . . . . . . 6 6 . . 6 6 . . . . . . 
+. . . . . . . . 6 6 . . 6 6 . . . . . . 
+. . . . . . . e e e . . e e e . . . . . 
+. . . . . . e e e e . . e e e e . . . . 
+`)
+                pause(100)
+                mySprite.setImage(img`
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . e e e e . . . . . . 
+. . . . . . . . . e e e e e e . . . . . 
+. . . . . . . . . e d e d e e . . . . . 
+. . . . . . . . . d d d d d e . . . . . 
+. . . . . . . . . d d d d d d . . . . . 
+. . . . . . . . . d d d d d d . . . . . 
+. . . . . . . . . . . d d . . . . . . . 
+. . . . . . . e e e e e e e e e e . . . 
+. . . . . . . e e e e e e e e e e . . . 
+. . . . . . . d . e e e e e e . d . . . 
+. . . . . . . d . e e e e e e . d . . . 
+. . . . . . b d . e e e e e e . d . . . 
+b b b b b b b d . e e e e e e . d . . . 
+. . . . . . b . . b b 5 5 b b . . . . . 
+. . . . . . . . . 6 6 6 6 6 6 . . . . . 
+. . . . . . . . . 6 6 . . 6 6 . . . . . 
+. . . . . . . . . 6 6 . . 6 6 . . . . . 
+. . . . . . . . e e e . . e e e . . . . 
+. . . . . . . e e e e . . e e e e . . . 
+`)
+                pause(100)
+                mySprite.setImage(img`
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . e e e e . . . . . . . 
+. . . . . . . . e e e e e e . . . . . . 
+. . . . . . . . e d e d e e . . . . . . 
+. . . . . . . . d d d d d e . . . . . . 
+. . . . . . . . d d d d d d . . . . . . 
+. . . . . . . . d d d d d d . . . . . . 
+. . . . . . . . . . d d . . . . . . . . 
+. . . . . . e e e e e e e e e e . . . . 
+. . . . . . e e e e e e e e e e . . . . 
+. . . . . . d . e e e e e e . d . . . . 
+. . . . . . d . e e e e e e . d . . . . 
+. . . . . . d . e e e e e e . d . . . . 
+. . . . b . d . e e e e e e . d . . . . 
+. . . . . b . . b b 5 5 b b . . . . . . 
+. . . . b . b . 6 6 6 6 6 6 . . . . . . 
+. . . b . . . . 6 6 . . 6 6 . . . . . . 
+. . b . . . . . 6 6 . . 6 6 . . . . . . 
+. b . . . . . e e e . . e e e . . . . . 
+. . . . . . e e e e . . e e e e . . . . 
+`)
+                pause(100)
+                if_waking = false
+                mySprite.setImage(img`
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . e e e e . . . . . . . . . 
+. . . . . . e e e e e e . . . . . . . . 
+. . . . . . e d e d e e . . . . . . . . 
+. . . . . . d d d d d e . . . . . . . . 
+. . . . . . d d d d d d . . . . . . . . 
+. . . . . . d d d d d d . . . . . . . . 
+. . . . . . . . d d . . . . . . . . . . 
+. . . . e e e e e e e e e e . . . . . . 
+. . . . e e e e e e e e e e . . . . . . 
+. . . . d . e e e e e e . d . . . . . . 
+. . . . d . e e e e e e . d . . . . . . 
+. . . . d . e e e e e e . d . . . . . . 
+. . . . d . e e e e e e . d . . . . . . 
+. . . . . . b b 5 5 b b . . . . . . . . 
+. . . . . . 6 6 6 6 6 6 . . . . . . . . 
+. . . . . . 6 6 . . 6 6 . . . . . . . . 
+. . . . . . 6 6 . . 6 6 . . . . . . . . 
+. . . . . e e e . . e e e . . . . . . . 
+. . . . e e e e . . e e e e . . . . . . 
+`)
+                pause(100)
+            }
+        }
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    if (sprite.x < otherSprite.x) {
+        if (if_waking == true) {
+            if (0 == 0) {
+            	
+            } else {
+            	
+            }
+        }
+    }
+})
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (controller.B.isPressed()) {
+        mySprite.startEffect(effects.trail, 500)
+        for (let index = 0; index < 20; index++) {
+            mySprite.y += -1
+            pause(1)
+        }
+    }
+})
+sprites.onCreated(SpriteKind.Enemy, function (sprite) {
+	
+})
+let mySprite3: Sprite = null
+let ww = 0
+let mySprite2: Sprite = null
+let list: tiles.Location[] = []
+let if_waking = false
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . e e e e . . . . . . . 
+. . . . e e e e e e . . . . . . 
+. . . . e d e d e e . . . . . . 
+. . . . d d d d d e . . . . . . 
+. . . . d d d d d d . . . . . . 
+. . . . d d d d d d . . . . . . 
+. . . . . . d d . . . . . . . . 
+. . e e e e e e e e e e . . . . 
+. . e e e e e e e e e e b . . . 
+. . d . e e e e e e . d b b . . 
+. . d . e e e e e e . d b b . . 
+. . d . e e e e e e . d b b . . 
+. . d . e e e e e e . d b b . . 
+. . . . b b 5 5 b b . . b b . . 
+. . . . 6 6 6 6 6 6 . . b b . . 
+. . . . 6 6 . . 6 6 . . b . . . 
+. . . . 6 6 . . 6 6 . . . . . . 
+. . . e e e . . e e e . . . . . 
+. . e e e e . . e e e e . . . . 
+`, SpriteKind.Player)
+controller.moveSprite(mySprite)
+tiles.setTilemap(tiles.createTilemap(
+            hex`1000100006060606060606060606060606060606060606060606060606060606060606060606060606060606060606060606060606060606060606060606060606060606060606060606060606060606060606060606060606060606060606060606060605050505050505050505050505050505050805050505050505050505050505050f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0505050505050505050505050505050505050505050505050505050505050505050505050505050805050505050505050505050505050505050505050505050505050505050505050505050505050505`,
+            img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0,sprites.castle.tilePath5,sprites.castle.tilePath2,sprites.castle.tilePath8,sprites.castle.tileGrass2,sprites.castle.tileGrass1,myTiles.tile1,myTiles.tile2,sprites.castle.tileGrass3,sprites.castle.tileDarkGrass3,sprites.builtin.forestTiles10,sprites.builtin.forestTiles6,sprites.builtin.forestTiles18,sprites.builtin.forestTiles27,myTiles.tile3,myTiles.tile4,sprites.builtin.forestTiles12,sprites.builtin.forestTiles8],
+            TileScale.Sixteen
+        ))
+tiles.placeOnTile(mySprite, tiles.getTileLocation(15, 15))
+scene.cameraFollowSprite(mySprite)
+if_waking = false
+enmy_span()
+game.onUpdateInterval(5000, function () {
+    mySprite2.follow(mySprite, 20)
+    mySprite3 = sprites.create(img`
+1 1 . . . . b . 
+. 1 1 e e e b b 
+1 1 . . . . b . 
+`, SpriteKind.Projectile)
+    mySprite3.setPosition(mySprite2.x, mySprite2.y)
+    mySprite3.setFlag(SpriteFlag.DestroyOnWall, true)
+    if (mySprite.x < mySprite2.x) {
+        mySprite3.vx = -50
+        mySprite3.image.flipX()
+    } else {
+        if (mySprite.x > mySprite2.x) {
+            mySprite3.vx = 50
+        } else {
+            if (mySprite.y > mySprite2.y) {
+                mySprite3.vy = 50
+            } else {
+                if (mySprite.y < mySprite2.y) {
+                    mySprite3.vy = -50
+                }
+            }
+        }
+    }
+})
+forever(function () {
+    if (controller.left.isPressed()) {
+        mySprite.setImage(img`
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . e e e e . . . . . . . . . 
+. . . . . . e e e e e e . . . . . . . . 
+. . . . . . e d e d e e . . . . . . . . 
+. . . . . . d d d d d e . . . . . . . . 
+. . . . . . d d d d d d . . . . . . . . 
+. . . . . . d d d d d d . . . . . . . . 
+. . . . . . . . d d . . . . . . . . . . 
+. . . . e e e e e e e e e e . . . . . . 
+. . . b e e e e e e e e e e . . . . . . 
+. . b b d . e e e e e e . d . . . . . . 
+. . b b d . e e e e e e . d . . . . . . 
+. . b b d . e e e e e e . d . . . . . . 
+. . b b d . e e e e e e . d . . . . . . 
+. . b b . . b b 5 5 b b . . . . . . . . 
+. . b b . . 6 6 6 6 6 6 . . . . . . . . 
+. . . b . . 6 6 . . 6 6 . . . . . . . . 
+. . . . . . 6 6 . . 6 6 . . . . . . . . 
+. . . . . e e e . . e e e . . . . . . . 
+. . . . e e e e . . e e e e . . . . . . 
+`)
+        ww = 1
+    } else {
+        if (controller.right.isPressed()) {
+            mySprite.setImage(img`
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . e e e e . . . . . . . . . 
+. . . . . . e e e e e e . . . . . . . . 
+. . . . . . e e d e d e . . . . . . . . 
+. . . . . . e d d d d d . . . . . . . . 
+. . . . . . d d d d d d . . . . . . . . 
+. . . . . . d d d d d d . . . . . . . . 
+. . . . . . . . d d . . . . . . . . . . 
+. . . . e e e e e e e e e e . . . . . . 
+. . . . e e e e e e e e e e b . . . . . 
+. . . . d . e e e e e e . d b b . . . . 
+. . . . d . e e e e e e . d b b . . . . 
+. . . . d . e e e e e e . d b b . . . . 
+. . . . d . e e e e e e . d b b . . . . 
+. . . . . . b b 5 5 b b . . b b . . . . 
+. . . . . . 6 6 6 6 6 6 . . b b . . . . 
+. . . . . . 6 6 . . 6 6 . . b . . . . . 
+. . . . . . 6 6 . . 6 6 . . . . . . . . 
+. . . . . e e e . . e e e . . . . . . . 
+. . . . e e e e . . e e e e . . . . . . 
+`)
+            ww = 2
+        }
+    }
+})
